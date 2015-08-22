1bd1e7cdc651f9dc2cd4e337fa3d44d26bdca56f
Wang-Jun-Chao <shining-glory@qq.com>
2015-08-22 08:58:13 +0800

    commit

--- 04-09-在执行器中取消任务/src/com/concurrency/task/Task.java
+++ 04-09-在执行器中取消任务/src/com/concurrency/task/Task.java
@@ -2,12 +2,6 @@ package com.concurrency.task;
 
 import java.util.concurrent.Callable;
 
-/**
- * Author: 王俊超
- * Date: 2014-11-30
- * Time: 15:03
- * Declaration: All Rights Reserved !!!
- */
 public class Task implements Callable<String> {
     /**
      * 核心方法，一个无限循环的任务，每100毫秒向控制台写一个消息
